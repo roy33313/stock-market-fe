e13fee372cec377f4f6f8801c6e4236eebdb5624
Royal <royalpinto333@gmail.com>
2023-04-22 08:29:12 +0530

    fix : Minor fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { isAuthenticated } from "../serverQueries/auth/auth";
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from "react";
 import NavBar from "../components/NavBar/NavBar";
 import LoginWidget from "../components/LoginWidget/LoginWidget";
