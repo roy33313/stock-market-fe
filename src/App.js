ef2d0f04955b839724b92a55183fb4da6472ac5a
Royal <royalpinto333@gmail.com>
2023-04-23 11:09:31 +0530

    contact us updated

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
+        <Route exact path="/" element={<Login />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/register" element={<Register />} />
         <Route exact path="/home" element={<Home />} />
--- src/components/ContactUsWidget/ContactUsWidget.js
+++ src/components/ContactUsWidget/ContactUsWidget.js
@@ -1,135 +1,73 @@
 import React from "react";
+import "./ContactUs.css";
+import bgImage from "../../assets/images/contactUs/bg.mp4";
 
 export default function ContactUsWidget() {
   return (
-    <div>
-      Contact us
-      <>
-        {/*Section: Contact v.2*/}
-        <section className="mb-4">
-          {/*Section heading*/}
-          <h2 className="h1-responsive font-weight-bold text-center my-4">
-            Contact us
-          </h2>
-          {/*Section description*/}
-          <p className="text-center w-responsive mx-auto mb-5">
-            Do you have any questions? Please do not hesitate to contact us
-            directly. Our team will come back to you within a matter of hours to
-            help you.
-          </p>
-          <div className="row">
-            {/*Grid column*/}
-            <div className="col-md-9 mb-md-0 mb-5">
-              <form
-                id="contact-form"
-                name="contact-form"
-                action="mail.php"
-                method="POST"
-              >
-                {/*Grid row*/}
-                <div className="row">
-                  {/*Grid column*/}
-                  <div className="col-md-6">
-                    <div className="md-form mb-0">
-                      <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        className="form-control"
-                      />
-                      <label htmlFor="name" className="">
-                        Your name
-                      </label>
-                    </div>
-                  </div>
-                  {/*Grid column*/}
-                  {/*Grid column*/}
-                  <div className="col-md-6">
-                    <div className="md-form mb-0">
-                      <input
-                        type="text"
-                        id="email"
-                        name="email"
-                        className="form-control"
-                      />
-                      <label htmlFor="email" className="">
-                        Your email
-                      </label>
-                    </div>
-                  </div>
-                  {/*Grid column*/}
-                </div>
-                {/*Grid row*/}
-                {/*Grid row*/}
-                <div className="row">
-                  <div className="col-md-12">
-                    <div className="md-form mb-0">
-                      <input
-                        type="text"
-                        id="subject"
-                        name="subject"
-                        className="form-control"
-                      />
-                      <label htmlFor="subject" className="">
-                        Subject
-                      </label>
-                    </div>
-                  </div>
-                </div>
-                {/*Grid row*/}
-                {/*Grid row*/}
-                <div className="row">
-                  {/*Grid column*/}
-                  <div className="col-md-12">
-                    <div className="md-form">
-                      <textarea
-                        type="text"
-                        id="message"
-                        name="message"
-                        rows={2}
-                        className="form-control md-textarea"
-                        defaultValue={""}
-                      />
-                      <label htmlFor="message">Your message</label>
-                    </div>
-                  </div>
-                </div>
-                {/*Grid row*/}
-              </form>
-              <div className="text-center text-md-left">
-                <a
-                  className="btn btn-primary"
-                  onclick="document.getElementById('contact-form').submit();"
-                  href="/"
-                >
-                  Send
-                </a>
-              </div>
-              <div className="status" />
+    <div class="container contact-form">
+      <div>
+        <video loop autoPlay muted id="bg-video">
+          <source src={bgImage} />
+        </video>
+      </div>
+      <div class="contact-image">
+        <img
+          src="https://image.ibb.co/kUagtU/rocket_contact.png"
+          alt="rocket_contact"
+        />
+      </div>
+      <form method="post">
+        <h3>Drop Us a Message</h3>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <input
+                type="text"
+                name="txtName"
+                class="form-control"
+                placeholder="Your Name *"
+                value=""
+              />
             </div>
-            {/*Grid column*/}
-            {/*Grid column*/}
-            <div className="col-md-3 text-center">
-              <ul className="list-unstyled mb-0">
-                <li>
-                  <i className="fas fa-map-marker-alt fa-2x" />
-                  <p>San Francisco, CA 94126, USA</p>
-                </li>
-                <li>
-                  <i className="fas fa-phone mt-4 fa-2x" />
-                  <p>+ 01 234 567 89</p>
-                </li>
-                <li>
-                  <i className="fas fa-envelope mt-4 fa-2x" />
-                  <p>contact@mdbootstrap.com</p>
-                </li>
-              </ul>
+            <div class="form-group">
+              <input
+                type="text"
+                name="txtEmail"
+                class="form-control"
+                placeholder="Your Email *"
+                value=""
+              />
             </div>
-            {/*Grid column*/}
+            <div class="form-group">
+              <input
+                type="text"
+                name="txtPhone"
+                class="form-control"
+                placeholder="Your Phone Number *"
+                value=""
+              />
+            </div>
+            <div class="form-group">
+              <input
+                type="submit"
+                name="btnSubmit"
+                class="btnContact"
+                value="Send Message"
+              />
+            </div>
           </div>
-        </section>
-        {/*Section: Contact v.2*/}
-      </>
+          <div class="col-md-6">
+            <div class="form-group">
+              <textarea
+                name="txtMsg"
+                class="form-control"
+                placeholder="Your Message *"
+                style={{ width: "100%", height: "150px" }}
+              ></textarea>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
