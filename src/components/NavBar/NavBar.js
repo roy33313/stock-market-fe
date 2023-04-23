33b154026d02da9d1dda453a344980d1490d0f70
Royal <royalpinto333@gmail.com>
2023-04-23 18:51:13 +0530

    fixed indentation

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -9,35 +9,28 @@ export default function NavBar() {
   }, []);
 
   return (
-    <nav className="navbar navbar-expand-lg" style={{backgroundColor:"#010c12"}}>
+    <nav
+      className="navbar navbar-expand-lg"
+      style={{ backgroundColor: "#010c12" }}
+    >
       <div className="container-fluid">
         <div className=" collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav  " >
-            <li className="nav-item" >
-              <Link
-                className="nav-link mx-2"
-                aria-current="page"
-                to="/home"
-              >
-                <p style={{color:"#FFFFFF"}}>
-                Home
-                </p>
+          <ul className="navbar-nav  ">
+            <li className="nav-item">
+              <Link className="nav-link mx-2" aria-current="page" to="/home">
+                <p style={{ color: "#FFFFFF" }}>Home</p>
               </Link>
             </li>
             {!flag ? (
               <>
                 <li className="nav-item">
                   <Link className="nav-link mx-2" to="/login">
-                  <p style={{color:"#FFFFFF"}}>
-                    Login
-                    </p>
+                    <p style={{ color: "#FFFFFF" }}>Login</p>
                   </Link>
                 </li>
                 <li className="nav-item">
                   <Link className="nav-link mx-2" to="/register">
-                  <p style={{color:"#FFFFFF"}}>
-                  Register
-                    </p>
+                    <p style={{ color: "#FFFFFF" }}>Register</p>
                   </Link>
                 </li>
               </>
@@ -45,44 +38,29 @@ export default function NavBar() {
               <>
                 <li className="nav-item">
                   <Link className="nav-link mx-2" to="/profile">
-                  <p style={{color:"#FFFFFF"}}>
-                  Profile
-                    </p>
+                    <p style={{ color: "#FFFFFF" }}>Profile</p>
                   </Link>
                 </li>
                 <li className="nav-item">
                   <Link className="nav-link mx-2" to="/home">
-                  <p style={{color:"#FFFFFF"}}>
-                  Predictions
-                    </p>
-                    
+                    <p style={{ color: "#FFFFFF" }}>Predictions</p>
                   </Link>
                 </li>
                 <li className="nav-item">
                   <Link className="nav-link mx-2" to="/news">
-                  <p style={{color:"#FFFFFF"}}>
-                  News
-                    </p>
-                    
+                    <p style={{ color: "#FFFFFF" }}>News</p>
                   </Link>
                 </li>
               </>
             )}
             <li className="nav-item">
               <Link className="nav-link mx-2" to="/aboutus">
-              <p style={{color:"#FFFFFF"}}>
-              About us
-                    </p>
-                    
-                
+                <p style={{ color: "#FFFFFF" }}>About us</p>
               </Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link mx-2" to="/contactus">
-              <p style={{color:"#FFFFFF"}}>
-              Contact us
-                    </p>
-                
+                <p style={{ color: "#FFFFFF" }}>Contact us</p>
               </Link>
             </li>
           </ul>
