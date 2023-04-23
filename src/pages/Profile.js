4b51251f00cdf537ce3fa1c5daf9496729d35a64
Royal <royalpinto333@gmail.com>
2023-04-23 15:30:16 +0530

    minor fix

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -10,6 +10,7 @@ export default function Profile() {
     if (!isAuthenticated()) {
       navigate("/login");
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
     <div>
