57d067246dd7846edfdb1cb59c7805876786b2d6
reinslo1 <reinaldlobo1@gmail.com>
2023-04-23 15:01:03 +0530

    Profile page Custom

--- src/components/ProfileWidget/ProfileWidget.js
+++ src/components/ProfileWidget/ProfileWidget.js
@@ -1,60 +1,62 @@
 import React from "react";
 
 export default function ProfileWidget() {
-  return (
-    <section style={{ backgroundColor: "#eee" }}>
-      <div className="container py-5">
-        <div className="row">
-          <div className="col-lg-12">
-            <div className="card mb-4">
-              <div className="card-body text-center">
-                <img
-                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"
-                  alt="avatar"
-                  className="rounded-circle img-fluid"
-                  style={{ width: 150 }}
-                />
-              </div>
-              <div className="row">
-                <div className="col-sm-3">
-                  <p className="mb-0">Full Name</p>
-                </div>
-                <div className="col-sm-9">
-                  <p className="text-muted mb-0">Johnatan Smith</p>
-                </div>
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-sm-3">
-                  <p className="mb-0">Email</p>
-                </div>
-                <div className="col-sm-9">
-                  <p className="text-muted mb-0">example@example.com</p>
-                </div>
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-sm-3">
-                  <p className="mb-0">Phone</p>
-                </div>
-                <div className="col-sm-9">
-                  <p className="text-muted mb-0">(097) 234-5678</p>
-                </div>
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-sm-3">
-                  <p className="mb-0">Mobile</p>
-                </div>
-                <div className="col-sm-9">
-                  <p className="text-muted mb-0">(098) 765-4321</p>
-                </div>
-              </div>
-              <hr />
+  return (<>
+  <section style={{ backgroundColor: "#010c12", color:"white"}}>
+  <div className="container py-5" >
+    <div className="row" >
+      <div className="col-lg-12" >
+          <div className="card-body text-center" >
+            <img
+              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"
+              alt="avatar"
+              className="rounded-circle img-fluid"
+              style={{ width: 150 }}
+            />
+          </div>
+          <div className="row">
+            <div className="col-sm-3">
+              <p className="mb-0">Full Name</p>
+            </div>
+            <div className="col-sm-9">
+              <p className="text mb-0">Johnatan Smith</p>
             </div>
           </div>
+          <hr />
+          <div className="row">
+            <div className="col-sm-3">
+              <p className="mb-0">Email</p>
+            </div>
+            <div className="col-sm-9">
+              <p className="text mb-0">example@example.com</p>
+            </div>
+          </div>
+          <hr />
+          <div className="row">
+            <div className="col-sm-3">
+              <p className="mb-0">Phone</p>
+            </div>
+            <div className="col-sm-9">
+              <p className="text mb-0">(097) 234-5678</p>
+            </div>
+          </div>
+          <hr />
+          <div className="row">
+            <div className="col-sm-3">
+              <p className="mb-0">Mobile</p>
+            </div>
+            <div className="col-sm-9">
+              <p className="text mb-0">(098) 765-4321</p>
+            </div>
+          </div>
+          <hr />
         </div>
       </div>
-    </section>
+    </div>
+</section>
+<div  style={{backgroundColor:"#02202B",height:"100vh"}}>
+</div>
+  </>
+    
   );
 }
