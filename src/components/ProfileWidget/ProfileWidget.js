c700a4cd447a2804ada6ba5aa3eb104f3e9bef14
rayan <you@example.com>
2023-04-26 08:23:42 +0530

    profile widget

--- src/components/ProfileWidget/ProfileWidget.js
+++ src/components/ProfileWidget/ProfileWidget.js
@@ -12,65 +12,68 @@ export default function ProfileWidget(props) {
   };
   return (
     <>
-      <section style={{ backgroundColor: "#010c12", color: "white" }}>
-        <div className="container py-5">
-          <div className="row">
-            <div className="col-lg-12">
-              <div className="card-body text-center">
-                <img
-                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"
-                  alt="avatar"
-                  className="rounded-circle img-fluid"
-                  style={{ width: 150 }}
-                />
+      <div style={{ height: "50%" }}>
+        {" "}
+        <section style={{ backgroundColor: "#010c12", color: "white" }}>
+          <div className="container py-5">
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="card-body text-center">
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"
+                    alt="avatar"
+                    className="rounded-circle img-fluid"
+                    style={{ width: 150 }}
+                  />
+                </div>
+                <div className="row">
+                  <div className="col-sm-3">
+                    <p className="mb-0">First Name</p>
+                  </div>
+                  <div className="col-sm-9">
+                    <p className="text mb-0">{props.details.firstName}</p>
+                  </div>
+                </div>
+                <hr />
+                <div className="row">
+                  <div className="col-sm-3">
+                    <p className="mb-0">Last Name</p>
+                  </div>
+                  <div className="col-sm-9">
+                    <p className="text mb-0">{props.details.lastName}</p>
+                  </div>
+                </div>
+                <hr />
+                <div className="row">
+                  <div className="col-sm-3">
+                    <p className="mb-0">Email</p>
+                  </div>
+                  <div className="col-sm-9">
+                    <p className="text mb-0">{props.details.email}</p>
+                  </div>
+                </div>
+                <hr />
+                <div className="row">
+                  <div className="col-sm-3">
+                    <p className="mb-0">Mobile</p>
+                  </div>
+                  <div className="col-sm-9">
+                    <p className="text mb-0">{props.details.phoneNo}</p>
+                  </div>
+                </div>
+                <hr />
               </div>
-              <div className="row">
-                <div className="col-sm-3">
-                  <p className="mb-0">First Name</p>
-                </div>
-                <div className="col-sm-9">
-                  <p className="text mb-0">{props.details.firstName}</p>
-                </div>
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-sm-3">
-                  <p className="mb-0">Last Name</p>
-                </div>
-                <div className="col-sm-9">
-                  <p className="text mb-0">{props.details.lastName}</p>
-                </div>
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-sm-3">
-                  <p className="mb-0">Email</p>
-                </div>
-                <div className="col-sm-9">
-                  <p className="text mb-0">{props.details.email}</p>
-                </div>
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-sm-3">
-                  <p className="mb-0">Mobile</p>
-                </div>
-                <div className="col-sm-9">
-                  <p className="text mb-0">{props.details.phoneNo}</p>
-                </div>
-              </div>
-              <hr />
             </div>
           </div>
+        </section>
+        <div
+          class="lastCont"
+          style={{ backgroundColor: "#02202B", height: "30vh" }}
+        >
+          <button onClick={handleLogout} type="button" class="btn btn-primary">
+            Logout
+          </button>
         </div>
-      </section>
-      <div
-        class="lastCont"
-        style={{ backgroundColor: "#02202B", height: "100vh" }}
-      >
-        <button onClick={handleLogout} type="button" class="btn btn-primary">
-          Logout
-        </button>
       </div>
     </>
   );
